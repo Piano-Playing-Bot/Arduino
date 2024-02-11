61edc6dfdb57f70fe2587f9e3d715a9ed8189991
ArtInLines <linesinart@gmail.com>
2024-02-11 21:40:21 +0100

    Fix communication with UI: FLUSH BUFFERS

--- arduino.ino
+++ arduino.ino
@@ -11,16 +11,30 @@ typedef int8_t   i8;
 #define LAST_OCTAVE_LEN KEYS_AMOUNT - (FULL_OCTAVES_AMOUNT*PIANO_KEY_AMOUNT + (PIANO_KEY_AMOUNT - STARTING_KEY))
 #define MID_OCTAVE_START_IDX (PIANO_KEY_AMOUNT - STARTING_KEY) + PIANO_KEY_AMOUNT*(FULL_OCTAVES_AMOUNT/2)
 #define MAX_KEYS_AT_ONCE 10 // The maximum amount of keys to play at once
-#define MIN_KEY_VAL 165 // Minimum value to set for a motor to move, if a key should be played
+#define MIN_KEY_VAL 185 // Minimum value to set for a motor to move, if a key should be played
 #define MAX_KEY_VAL 255 // Maximum value to set for a motor to move, if a key should be played
-#define CLOCK_CYCLE_LEN 8 // in milliseconds
+#define CLOCK_CYCLE_LEN 88 // in milliseconds
+#define SHIFT_REGISTER_COUNT 1
 
-ShiftRegisterPWM sr(1, 16);
+ShiftRegisterPWM sr(SHIFT_REGISTER_COUNT, 16);
+
+// typedef struct {
+//     u64  time;   // The clock-cycle on which to start playing this note
+//     u16  len;    // The amount of clock cycles, that playing this note should take
+//     PianoKey  key;    // The note's key
+//     i8   octave; // Which octave the key should be played on (zero is the middle octave)
+//     bool on;     // Whether the note should be played (true) or not (false)
+// } MusicChunk;
 
 u8 keyValues[KEYS_AMOUNT] = {0}; // This array is adressing the motors for each key on the piano
-AIL_DA(MusicChunk) musicChunks;
+MusicChunk musicChunks[MAX_CHUNKS_AMOUNT] = {
+    { .time = 1, .len = 90, .key = PIANO_KEY_A, .octave = -16, .on = true },
+    { .time = 2, .len = 90, .key = PIANO_KEY_A, .octave = -16, .on = false },
+};
+u32 musicChunksLen  = 2;
 u32 chunkIdx        = 0;
 u64 musicTime       = 0;
+u16 restTime        = 0;
 u8  activeKeysCount = 0;
 bool isMusicPlaying = false;
 
@@ -36,16 +50,11 @@ u32 remainingLen      = 0;
 u32 jumpTime          = 0;
 
 void setup() {
-    musicChunks.data = NULL;
-    musicChunks.cap  = 0;
-    musicChunks.len  = 0;
-    musicChunks.allocator = &ail_default_allocator;
-
     pinMode(LED_BUILTIN, OUTPUT); // @Cleanup for debugging only
 
     pinMode(2, OUTPUT); // sr data pin
     pinMode(3, OUTPUT); // sr clock pin
-    pinMode(4, OUTPUT); // sr latch pin
+    pinMode(4, OUTPUT); // sr ST_CP pin
     sr.interrupt(ShiftRegisterPWM::UpdateFrequency::SuperFast);
 
     // initialize serial port
@@ -53,6 +62,18 @@ void setup() {
     while (!Serial) {
       ; // wait for serial port to connect. Needed for native USB port only
     }
+}
+
+void printKeyVals()
+{
+    Serial.println("Test");
+    // Serial.print("[");
+    // Serial.print(keyValues[0], DEC);
+    // for (u8 i = 0; i < KEYS_AMOUNT; i++) {
+    //     Serial.print(", ");
+    //     Serial.print(keyValues[i]);
+    // }
+    // Serial.print("]\n");
 }
 
 // @Cleanup: Only exists for debugging
@@ -93,7 +114,7 @@ void writeMsg(ServerMsgType type) {
     ail_buf_write4msb(&replyBuffer, type);
     ail_buf_write4lsb(&replyBuffer, 0);
     Serial.write((u8 *)reply, 12);
-    blink(6);
+    Serial.flush();
 }
 
 void clearPrevMsg() {
@@ -144,7 +165,7 @@ void loop() {
                 case MSG_PIDI: {
                     u32 n = (msgBuffer.len - msgBuffer.idx)/ENCODED_MUSIC_CHUNK_LEN;
                     for (u32 i = 0; i < n; i++) {
-                        ail_da_push(&musicChunks, decode_chunk(&msgBuffer));
+                        musicChunks[musicChunksLen++] = decode_chunk(&msgBuffer);
                     }
                     remainingLen -= n*ENCODED_MUSIC_CHUNK_LEN;
                     AIL_ASSERT(remainingLen % ENCODED_MUSIC_CHUNK_LEN == 0);
@@ -185,8 +206,8 @@ void loop() {
                 clearMotors();
                 Serial.print("Jumping to time: ");
                 Serial.println(jumpTime);
-                for (u32 i = 0; i < musicChunks.len && musicChunks.data[i].time <= jumpTime; i++) {
-                    print_chunk(musicChunks.data[i]);
+                for (u32 i = 0; i < musicChunksLen && musicChunks[i].time <= jumpTime; i++) {
+                    print_chunk(musicChunks[i]);
                     // @TODO: Apply Chunk
                 }
                 writeMsg(MSG_SUCC);
@@ -197,20 +218,27 @@ void loop() {
         msgType = MSG_NONE;
     }
 
+    // Motor on PIN 5
+    // LED on PIN 2
+    // sr.set(1, 0);
+    // sr.set(0, 0);
+    for (u8 i = 0; i < 8; i++) sr.set(i, 185);
+
     // Play Music
     if (isMusicPlaying) {
         // Check if music is done
-        if (chunkIdx >= musicChunks.len) {
+        // Serial.println(chunkIdx);
+        if (chunkIdx >= musicChunksLen) {
             clearMotors();
             // to play in a loop: chunkIdx = 0;
             // otherwise setting isMusicPlaying = false; might be worthwile
-            break;
+            goto done;
         }
 
         // Update keyValues array
-        while (musicChunks.data[chunkIdx].time <= musicTime) {
+        while (musicChunks[chunkIdx].time <= musicTime) {
             AIL_STATIC_ASSERT(KEYS_AMOUNT <= INT8_MAX);
-            MusicChunk chunk = musicChunks.data[chunkIdx];
+            MusicChunk chunk = musicChunks[chunkIdx];
             i8 key = MID_OCTAVE_START_IDX + PIANO_KEY_AMOUNT*chunk.octave + chunk.key;
             if (key < 0) key = (chunk.key < STARTING_KEY)*(PIANO_KEY_AMOUNT - STARTING_KEY) + chunk.key;
             else key = KEYS_AMOUNT - LAST_OCTAVE_LEN - (chunk.key >= LAST_OCTAVE_LEN)*PIANO_KEY_AMOUNT + chunk.key;
@@ -219,8 +247,10 @@ void loop() {
                 if      ( chunk.on && !keyValues[key]) activeKeysCount++;
                 else if (!chunk.on &&  keyValues[key]) activeKeysCount--;
                 keyValues[key]   = chunk.on*AIL_LERP(chunk.len/MAX_VELOCITY, MIN_KEY_VAL, MAX_KEY_VAL);
-                chunkIdx++;
             }
+            
+            chunkIdx++;
+            // printKeyVals();
         }
 
         // @TODO: Adress motors using keyValues array
@@ -246,12 +276,14 @@ void loop() {
         // }
         // delay(1000);
 
+      done:
+      // @TODO: Without delay with rest-time variable
         u64 elapsed = millis() - start;
-        if (elapsed < clock) {
+        musicTime += elapsed/CLOCK_CYCLE_LEN;
+        restTime  += elapsed%CLOCK_CYCLE_LEN;
+        if (restTime >= CLOCK_CYCLE_LEN) {
             musicTime++;
-            delay(clock - elapsed - 1);
-        } else {
-            musicTime += 1; + elapsed/CLOCK_CYCLE_LEN;
+            restTime -= CLOCK_CYCLE_LEN;
         }
     }
 }
